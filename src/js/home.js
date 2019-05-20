7746110730842a087d0f22c186f51a1b2d1051e2
Jehaann Manzanares <jehaann07carlos@gmail.com>
2019-05-20 00:14:05 -0600

    complete Animations

--- src/js/home.js
+++ src/js/home.js
@@ -1,3 +1,20 @@
-function modal(){
+const $overlay = document.getElementById('overlay')
+const $modal = document.getElementById('modal')
 
+function modal(){
+    
+    
+    $overlay.classList.add('active')
+    // $modal.classList.add('active')
+    $modal.style.animation = 'fadeInBottom 1s forwards'
+    
+    
 }
+
+
+$overlay.addEventListener('click', () => {
+    
+    $modal.style.animation = 'fadeOutBottom 1s forwards'
+    $overlay.style.transitionDelay = '1s';
+    $overlay.classList.remove('active')
+})

abba0c3c85b048568f58f147c9551d79401a43c1
Jehaann Manzanares <jehaann07carlos@gmail.com>
2019-05-25 22:42:32 -0600

    Connected to API

--- src/js/home.js
+++ src/js/home.js
@@ -18,3 +18,70 @@ $overlay.addEventListener('click', () => {
     $overlay.style.transitionDelay = '1s';
     $overlay.classList.remove('active')
 })
+
+
+async function load(){
+
+const $listCharacters = document.getElementById('listCharacters')
+
+async function getDataCharacters(url){
+
+    const response = await fetch(url)
+    const data = await response.json()
+
+    return data
+}
+
+function renderCharactersList(list, $container){
+
+    list.forEach(Character => {
+         const HTMLString = CharactersTemplate(Character)
+         const CharacterElement = createTemplate(HTMLString)
+
+         $container.append(CharacterElement)
+    });
+}
+
+function CharactersTemplate(Character){
+    const description = cutText(Character.description)
+    return(
+        `
+        <section class="list-character-container">
+                    <figure class="list-character-image">
+                        <img src="${Character.thumbnail.path}.${Character.thumbnail.extension}" alt="">
+                    </figure>
+                    <div class="list-character-texts">
+                        <header class="header-card">
+                            <h1 class="titles">${Character.name}</h1>
+                            
+                        </header>
+                        <div class="body-text">
+                            <p class="character-description">${description}...</p>
+                        </div>
+                        <footer class="list-character-btnArrow">
+                            <p >More info</p>
+                            <img class="icon" src="src/images/arrow.png" alt="" onclick="modal()">
+                        </footer>
+                    </div>
+        </section>
+        `
+    )
+}
+function cutText(text){
+    return text.substring(0,130)
+}
+function createTemplate(HTMLStrig){
+    const html = document.implementation.createHTMLDocument()
+    html.body.innerHTML = HTMLStrig
+    
+    return html.body.children[0]
+}
+
+const URL_API = 'https://gateway.marvel.com:443/v1/public/characters?&limit=10&apikey=5e0b7a3b8c23938b449a5da5f3584ca9'
+
+const {data: {results : Characters} } = await getDataCharacters(URL_API)
+renderCharactersList(Characters,$listCharacters)
+
+}
+
+load()

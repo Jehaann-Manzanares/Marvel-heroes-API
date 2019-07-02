ecd1d059644dfcdf79d8090ed811b6639fb2da56
Jehaann Manzanares <developer@jehaann-manzanares.com>
2019-07-01 23:09:22 -0600

    adding fuctional components

--- src/components/Home.jsx
+++ src/components/Home.jsx
@@ -4,15 +4,13 @@ import "./css/animations.css";
 import Header from "./Header";
 import ListCharacters from "./ListCharacters";
 
-class Home extends React.Component {
-  render() {
-    return (
-      <React.Fragment>
-        <Header />
-        <ListCharacters />
-      </React.Fragment>
-    );
-  }
+function Home() {
+  return (
+    <React.Fragment>
+      <Header />
+      <ListCharacters />
+    </React.Fragment>
+  );
 }
 
 export default Home;
--- src/index.js
+++ src/index.js
@@ -1,11 +1,5 @@
 import React from "react";
 import ReactDOM from "react-dom";
 import Home from "./components/Home";
-import * as serviceWorker from "./serviceWorker";
 
 ReactDOM.render(<Home />, document.getElementById("root"));
-
-// If you want your app to work offline and load faster, you can change
-// unregister() to register() below. Note this comes with some pitfalls.
-// Learn more about service workers: https://bit.ly/CRA-PWA
-serviceWorker.unregister();
